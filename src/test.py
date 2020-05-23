6d008a0313a567155e02eaf79f36561e50d6801c
hwangdav000 <hwangdav000@gmail.com>
2020-05-23 22:15:04 +0000

    test functionality

---
+++ src/test.py
@@ -0,0 +1,10 @@
+import translate
+
+test = "Hello my name is David"
+
+target = "es"
+
+translation = translate.GetTranslation(test,target)
+print(translation)
+
+
--- src/translate.py
+++ src/translate.py
@@ -1,8 +1,10 @@
 import boto3
 
-client = boto3.client('translate', region_name='us-east-1')
-text = "hola mi nombre es Noah"
 
-result = client.translate_text(Text=text, SourceLanguageCode ="auto", TargetLanguageCode="en")
-
-print(result['TranslatedText'])
+#translate from english to target language
+#returns translated text
+# en is english and es is spanish
+def GetTranslation(text, target):
+    client = boto3.client('translate', region_name='us-east-1')
+    result = client.translate_text(Text=text, SourceLanguageCode ="en", TargetLanguageCode=target)
+    return (result['TranslatedText'])
